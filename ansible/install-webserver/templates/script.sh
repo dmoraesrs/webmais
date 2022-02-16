79d6f6ac9ab4f2e9f783b8e781b40e38aebabda5
Diego Moraes <diego.moraes@fatecnologia.com>
2022-02-16 19:51:24 -0300

    add script

---
+++ ansible/install-webserver/templates/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+LOG=$(blkid |grep log |cut -d " " -f2 |cut -c 7-42)
+APP=$(blkid |grep app-app |cut -d " " -f2 |cut -c 7-42)
+echo "UUID=$LOG /var/log/apache2                   xfs    defaults        0       2" >> /etc/fstab
+echo "UUID=$APP /var/www                   xfs    defaults        0       2" >> /etc/fstab
